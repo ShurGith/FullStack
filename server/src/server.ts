c32a6c7e48839162cc289c7f1ce98cb694a638b0
ShurGith <75266693+ShurGith@users.noreply.github.com>
2025-08-11 01:04:56 +0200

    Acabado el dia

--- server/src/server.ts
+++ server/src/server.ts
@@ -3,25 +3,42 @@ import router from './router';
 import db from './config/db';
 import colors from 'colors';
 import swaggerUi from 'swagger-ui-express';
-import swaggerSpec, {swaggerUiOptions} from './config/swagger';
+import swaggerSpec, { swaggerUiOptions } from './config/swagger';
+import cors, { CorsOptions } from 'cors';
 
 
 
 //Conectar a la base de datos
 async function connectDB() {
-    try {
-        await db.authenticate();
-        db.sync();
-        console.log(colors.bgBlue.italic('Conexion exitosa a la Base de Datos.'));
-    } catch (error) {
-        console.log(colors.bgRed.underline.red.bold( "Hubo un error al conectar con la Base de Datos."));
-    }
+  try {
+    await db.authenticate();
+    db.sync();
+    console.log(colors.bgBlue.italic('Conexion exitosa a la Base de Datos.'));
+  } catch (error) {
+    console.log(colors.bgRed.underline.red.bold("Hubo un error al conectar con la Base de Datos."));
+  }
 }
 connectDB();
 
 const path = require('path');
 //Instancia de Express
 const server = express();
+
+//?Permitir conexiones y configurar cors
+const corsOptions: CorsOptions = {
+  origin: function (origin, callback) {
+    if (!origin || process.env.FRONTEND_URL) {
+      return callback(null, true);
+    }
+    else {
+      const msg = `La pagina ${origin} no esta autorizada para conectarse`;
+      return callback(new Error(msg), false)
+    }
+  }
+}
+
+server.use(cors(corsOptions));
+
 
 //Leer datos de formularios del body
 server.use(express.json());
